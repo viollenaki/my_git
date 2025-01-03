5b81d0e10d7ac3f6ff766a76ace70dcf138881df
viollenaki <tanakaakuustik@gmail.com>
2025-01-03 19:41:24 +0600

    update

--- first.py
+++ first.py
@@ -1,1 +1,4 @@
-print('hello world')
+with open(r"C:\Users\IdeaPad Pro 5i\Downloads\wallpapersden.com_windows-11-black_5120x3200.jpg", "rb") as foto:
+    with open("foto.jpg", "wb") as foto_kopya:
+        for satir in foto:
+            foto_kopya.write(satir)
